0a8b8dc5344d0305d9480140c64f5e0a2d13d445
t6869 <t6869@yandex.ru>
2022-04-14 14:26:31 +0300

    add modal

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { Postlist } from './components/PostList';
 import {PostFilter} from "./components/PostFilter";
 import './styles/App.css'
 import {PostForm} from "./components/PostForm";
+import {MyModal} from "./components/UI/MyModal/MyModal";
+import {MyBytton} from "./components/UI/button/MyButton";
+
 
 function App() {
   const [posts, setPosts] = useState([
-    { id: 1, title: 'aaa', body: 'eee' },
+    { id: 1, title: 'aa11a', body: 'eee' },
     { id: 2, title: 'bbb', body: 'tyi' },
     { id: 3, title: 'ddd', body: 'asc' },
   ]);
   const [filter, setFilter] = useState({sort: '', query: ''});
-
+  const [modal, setModal] = useState(false);
     //-?
   const sortedPosts = useMemo(()=>{
       console.log('Сортировка сработала')
@@ -25,9 +28,10 @@ function App() {
       return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()));
   },[filter.query, sortedPosts]);
 
-  const createPost = (newPost) => (
-    setPosts([...posts, newPost])
-  );
+  const createPost = (newPost) => {
+      setPosts([...posts, newPost]);
+      setModal(false);
+  };
 
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
@@ -35,7 +39,12 @@ function App() {
 
   return (
     <div className="App">
-        <PostForm createPost={createPost}/>
+        <MyBytton style={{marginTop: 30 }} onClick={() => setModal(true)}>
+            Создать пользователя
+        </MyBytton>
+        <MyModal visible={modal} setVisible={setModal}>
+            <PostForm createPost={createPost}/>
+        </MyModal>
         <hr style={{ margin: '15px 0' }} />
         <PostFilter
             filter={filter}
